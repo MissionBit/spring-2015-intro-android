e1fb80f00d6a85c405abcad9a5ea5e8772562adc
Danny Roa <dannyroa@gmail.com>
2015-03-22 10:20:49 -0700

    initial commit

---
+++ app/src/main/java/com/missionbit/spring2015introandroid/models/InstagramLocation.java
@@ -0,0 +1,9 @@
+package com.missionbit.spring2015introandroid.models;
+
+/**
+ * Created by dannyroa on 3/22/15.
+ */
+public class InstagramLocation {
+
+    String url;
+}
---
+++ app/src/main/java/com/missionbit/spring2015introandroid/models/InstagramPhoto.java
@@ -0,0 +1,7 @@
+package com.missionbit.spring2015introandroid.models;
+
+/**
+ * Created by dannyroa on 3/22/15.
+ */
+public class InstagramPhoto {
+}
